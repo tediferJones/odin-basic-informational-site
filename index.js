83b731026f8a845ebdf7f74cf94751ca10d9c763
ted <theo_drz@live.com>
2022-07-14 16:48:35 -0400

    converted to express

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile('index.html', (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    })
-  } else if (req.url === '/about') {
-    fs.readFile('about.html', (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    })
-  } else if (req.url === '/contact-me') {
-    fs.readFile('contact-me.html', (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    })
-  } else {
-    fs.readFile('404.html', (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
-    })
-  }
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
 })
 
-const PORT = 8080;
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact-me.html'));
+})
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '404.html'));
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+// OLD VERSION
+
+// const http = require('http');
+// const fs = require('fs');
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     fs.readFile('index.html', (err, content) => {
+//       if (err) throw err;
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(content);
+//     })
+//   } else if (req.url === '/about') {
+//     fs.readFile('about.html', (err, content) => {
+//       if (err) throw err;
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(content);
+//     })
+//   } else if (req.url === '/contact-me') {
+//     fs.readFile('contact-me.html', (err, content) => {
+//       if (err) throw err;
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(content);
+//     })
+//   } else {
+//     fs.readFile('404.html', (err, content) => {
+//       if (err) throw err;
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.end(content);
+//     })
+//   }
+// })
+
+// const PORT = 8080;
+
+// server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
